2740dec36e0641a6a74339baa28ed7d5f1bbc04d
cAptive <70532837+cAptive5976@users.noreply.github.com>
2023-10-30 10:10:00 +0100

    Update test_occupation.py

--- tests/test_occupation.py
+++ tests/test_occupation.py
@@ -1,1 +1,19 @@
-
+f = open("index.html",'w',encoding='utf-8')
+HTML_INDEX="""
+<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+<!DOCTYPE html>
+<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\" dir=\"ltr\">
+<style>
+   table, th, td {
+   border:1px solid black;
+   }
+</style>
+<head>
+<title> Mon titre </title>
+</head>
+<body>
+ Bonjour HTML
+</body>
+</html>"""
+f.write(HTML_INDEX)
+f.close
