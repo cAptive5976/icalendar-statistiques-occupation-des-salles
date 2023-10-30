34abea55e2f8510b3c88e501b87d6fb7f41ede8e
cAptive <70532837+cAptive5976@users.noreply.github.com>
2023-10-30 11:26:59 +0100

    Update test_occupation.py

--- tests/test_occupation.py
+++ tests/test_occupation.py
@@ -1,19 +1,29 @@
-f = open("index.html",'w',encoding='utf-8')
-HTML_INDEX="""
-<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
-<!DOCTYPE html>
-<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\" dir=\"ltr\">
-<style>
-   table, th, td {
-   border:1px solid black;
-   }
-</style>
-<head>
-<title> Mon titre </title>
-</head>
-<body>
- Bonjour HTML
-</body>
-</html>"""
+def genere_page_web(nom_fichier, titre_page, corps):
+    HTML_INDEX = """
+   <!DOCTYPE html>
+   <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\" dir=\"ltr\">
+   <head>
+   <title>{titre_page}</title>
+   <style>
+      table, th, td {{
+      border:1px solid black;
+      }}
+   </style>
+   </head>
+   <body>
+   {corps}
+   </body>
+   </html>
+   """
+f = open(nom_fichier,'w',encoding='utf-8')
 f.write(HTML_INDEX)
 f.close
+    
+
+def main():
+    corps = """ <h1> Titre </h1>
+            """
+    genere_page_web("../html/index.html", "mon titre", corps)
+
+if __name__ == "__main__":
+    main()
