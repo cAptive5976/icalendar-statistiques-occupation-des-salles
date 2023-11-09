b241b054928d314fc37ea35c45abffc4bc5f4894
etudiant <etudiant@deb12-vdi-rt>
2023-11-09 10:30:26 +0100

    Envoi des donnée du fichier python de test vers le vrai fichier python

--- occupation/occupation.py
+++ occupation/occupation.py
@@ -1,1 +1,28 @@
-input
+def genere_page_web(nom_fichier, titre_page, corps):
+    f = open(nom_fichier,'w',encoding='utf-8')
+    HTML_INDEX = """<!DOCTYPE html>
+   <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\" dir=\"ltr\">
+   <head>
+   <title>{titre_page}</title>
+   <style>
+      table, th, td {{
+      border:1px solid black;
+      }}
+   </style>
+   </head>
+   <body>
+   {corps}
+   </body>
+   </html>
+   """
+    f.write(HTML_INDEX)
+    f.close
+    
+
+def main():
+    corps = """ <h1> Titre </h1>
+            """
+    genere_page_web("../html/index.html", "mon titre", corps)
+
+if __name__ == "__main__":
+    main()
