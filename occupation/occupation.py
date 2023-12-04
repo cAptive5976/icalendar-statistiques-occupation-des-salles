95c3b5bbaf9ec3d201a7e199538b016d790ecf64
cAptive5976 <70532837+cAptive5976@users.noreply.github.com>
2023-12-04 13:50:53 +0100

    Test de premier module d'occupation

--- occupation/occupation.py
+++ occupation/occupation.py
@@ -1,9 +1,13 @@
+import module_occupation
+import datetime
+import argparse
+
 def genere_page_web(nom_fichier, titre_page, corps):
     f = open(nom_fichier,'w',encoding='utf-8')
     HTML_INDEX = """<!DOCTYPE html>
    <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\" dir=\"ltr\">
    <head>
-   <title>{titre_page}</title>
+   <title>{}</title>
    <style>
       table, th, td {{
       border:1px solid black;
@@ -11,10 +15,10 @@ def genere_page_web(nom_fichier, titre_page, corps):
    </style>
    </head>
    <body>
-   {corps}
+   {}
    </body>
    </html>
-   """
+   """.format(titre_page, corps)
     f.write(HTML_INDEX)
     f.close
     
