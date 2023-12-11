51916c97ae90376539403baa0076c9c4a8556b16
William Lamberton <etudiant@deb12-vdi-rt>
2023-12-11 11:39:37 +0100

    création d'une base de documentation avec sphinx #4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,11 @@ release = '0.5'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.todo',
+'sphinx.ext.mathjax',
+'sphinx.ext.ifconfig',
+'sphinx.ext.autodoc',
+'sphinx.ext.viewcode',]
 
 templates_path = ['_templates']
 exclude_patterns = []
