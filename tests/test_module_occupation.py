a66a12f191fd39a05a649b60ca2bd8c59c72722b
Charles <70532837+cAptive5976@users.noreply.github.com>
2023-12-30 15:31:43 +0100

    Ajout des tests unitaire pour process_data et generate_html #16

--- tests/test_module_occupation.py
+++ tests/test_module_occupation.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 from occupation import module_occupation
+from datetime import datetime
 
 class TestOccupation(unittest.TestCase):
     def test_extract_data(self):
@@ -17,6 +18,25 @@ class TestOccupation(unittest.TestCase):
         self.assertEqual(result[1], 'Calendrier BUT2')
         os.remove('test_ADECal_BUT2.ics')
         os.remove('test_ADECal_BUT1.ics')
+    def test_process_data(self):
+        data = ["BEGIN:VEVENT\nSUMMARY:Event 1\nLOCATION:Room A\nDTSTART:20220101T120000Z\nDTEND:20220101T130000Z\nEND:VEVENT",
+                "BEGIN:VEVENT\nSUMMARY:Event 2\nLOCATION:Room B\nDTSTART:20220102T120000Z\nDTEND:20220102T130000Z\nEND:VEVENT"]
+        processed_data = module_occupation.process_data(data)
+        self.assertEqual(len(processed_data), 2)
+        self.assertEqual(processed_data[0]['summary'], 'Event 1')
+        self.assertEqual(processed_data[1]['summary'], 'Event 2')
+
+    def test_generate_html(self):
+        data = [{"location": "Room A", "start_time": datetime(2022, 1, 1, 12, 0), "end_time": datetime(2022, 1, 1, 13, 0)},
+                {"location": "Room B", "start_time": datetime(2022, 1, 2, 12, 0), "end_time": datetime(2022, 1, 2, 13, 0)}]
+        output_dir = 'output/'
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        module_occupation.generate_html(data, output_dir)
+        html_file_path = os.path.join(output_dir, 'index.html')
+        self.assertTrue(os.path.exists(html_file_path))
+        os.remove(html_file_path)
+        os.rmdir(output_dir)
         
 
 if __name__ == '__main__':
